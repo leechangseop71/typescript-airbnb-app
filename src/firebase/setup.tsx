2dbe0b088bd16be015ae4514703f9c838598ec45
leechangseop71 <haseuki71@gmail.com>
2024-05-02 14:28:36 +0900

    api - .env

--- src/firebase/setup.tsx
+++ src/firebase/setup.tsx
@@ -1,16 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth, GoogleAuthProvider} from "firebase/auth"
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDMCK4fytQZ-OwuMY3m8tZAqlvfZxWurNY",
-  authDomain: "airbnb-clone-6bc3a.firebaseapp.com",
-  projectId: "airbnb-clone-6bc3a",
-  storageBucket: "airbnb-clone-6bc3a.appspot.com",
-  messagingSenderId: "933428158478",
-  appId: "1:933428158478:web:67efb02fe8b274077b9c5f"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app)
-export const googleAuthProvider = new GoogleAuthProvider()
+export const auth = getAuth(app);
+export const googleAuthProvider = new GoogleAuthProvider();
