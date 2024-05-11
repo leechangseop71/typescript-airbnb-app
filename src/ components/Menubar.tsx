94b327535266ec5a3939fa64d0099dd08203b589
leechangseop71 <haseuki71@gmail.com>
2024-05-11 12:28:25 +0900

    update...

--- src/ components/Menubar.tsx
+++ src/ components/Menubar.tsx
@@ -1,222 +1,37 @@
-import React from 'react'
-import Countryside from '../images/countryside.png'
-import Amazingpools from '../images/amazingpools.png'
-import Nationalparks from '../images/nationalparks.png'
-import Castles from '../images/castles.png'
-import Beachfront from '../images/beachfront.png'
-import Amazingviews from '../images/amazingviews.png'
-import Camping from '../images/camping.png'
-import Hanoks from '../images/hanoks.png'
-import Farms from '../images/rooms.png'
-import Rooms from '../images/rooms.png'
-import OMG from '../images/omg.png'
-import Lake from '../images/lake.png'
-import Trending from '../images/trending.png'
-import Design from '../images/design.png'
-import Cabins from '../images/cabins.png'
-import Tinyhomes from '../images/tinyhomes.png'
-import Play from '../images/play.png'
-import Grandpianos from '../images/grandpianos.png'
-import Tropcal from '../images/tropical.png'
-import Bedbreakfasts from '../images/bed&breakfasts.png'
-import Historicalhomes from '../images/historicalhomes.png'
-import Offthegrid from '../images/off-the-grid.png'
-import Treehouses from '../images/treehouses.png'
-import Lakefront from '../images/lakefront.png'
-import New from '../images/new.png'
-import Mansions from '../images/mansions.png'
-import Topcities from '../images/topcities.png'
-import Vineyards from '../images/Vineyards.png'
-import Luxe from '../images/Luxe.png'
-import Creativespaces from '../images/Creativespaces.png'
-import Islands from '../images/countryside.png'
-import Ryokans from '../images/countryside.png'
-import Houseboats from '../images/countryside.png'
-import Caves from '../images/countryside.png'
-import Boats from '../images/Boats.png'
-import Domes from '../images/Domes.png'
-import Aframes from '../images/aframes.png'
-import Arctic from '../images/arctic.png'
-import Containers from '../images/containers.png'
-import Skiinout from '../images/ski-in:out.png'
-import Golfing from '../images/golfing.png'
-import Towers from '../images/towers.png'
-import Earthhomes from '../images/earthhomes.png'
-import Topoftheworld from '../images/topoftheworld.png'
-import Surfing from '../images/surfing.png'
-
+import React from 'react';
+import Slider from 'react-slick'; // 수정: Slider 컴포넌트의 이름을 소문자에서 대문자로 변경
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Countryside from '../images/countryside.png';
+import Amazingpools from '../images/amazingpools.png';
+import Nationalparks from '../images/nationalparks.png';
 
 export const Menubar = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 1
+  };
+
   return (
-    <div className='flex pt-8'>
-        <div className='ml-9'>
-            <img src={Countryside} className='w-6 h-6'/>
-            <h1 className='text-xs font-semibold'>Countryside</h1>
-        </div>
-        <div className='ml-9'>
-            <img src={Amazingpools} className='w-6 h-6'/>
-            <h1 className='text-xs font-semibold'>Amazingpools</h1>
-        </div>
-        <div className='ml-9'>
-            <img src={Nationalparks} className='w-6 h-6'/>
-            <h1 className='text-xs font-semibold'>Nationalparks</h1>
-        </div>
-        <div className='ml-9'>
-            <img src={Castles} className='w-6 h-6'/>
-            <h1 className='text-xs font-semibold'>Castles</h1>
-        </div>
-        <div className='ml-9'>
-            <img src={Beachfront} className='w-6 h-6'/>
-            <h1 className='text-xs font-semibold'>Beachfront</h1>
-        </div>
-        <div className='ml-9'>
-            <img src={Amazingviews} className='w-6 h-6'/>
-            <h1 className='text-xs font-semibold'></h1>
-        </div>
-        <div className='ml-9'>
-            <img src={Camping} className='w-6 h-6'/>
-            <h1 className='text-xs font-semibold'></h1>
-        </div>
-        <div className='ml-9'>
-            <img src={Hanoks} className='w-6 h-6'/>
-            <h1 className='text-xs font-semibold'></h1>
-        </div>
-        <div className='ml-9'>
-            <img src={Farms} className='w-6 h-6'/>
-            <h1 className='text-xs font-semibold'></h1>
-        </div>
-        <div className='ml-9'>
-            <img src={Rooms} className='w-6 h-6'/>
-            <h1 className='text-xs font-semibold'></h1>
-        </div>
-        <div className='ml-9'>
-            <img src={OMG} className='w-6 h-6'/>
-            <h1 className='text-xs font-semibold'></h1>
-        </div>
-        <div className='ml-9'>
-            <img src={Lake} className='w-6 h-6'/>
-            <h1 className='text-xs font-semibold'></h1>
-        </div>
-        <div className='ml-9'>
-            <img src={Tinyhomes} className='w-6 h-6'/>
-            <h1 className='text-xs font-semibold'></h1>
-        </div>
-        <div className='ml-9'> 
-            <img src={Play} className='w-6 h-6'/>
-            <h1 className='text-xs font-semibold'></h1>
-        </div>
-        <div className='ml-9'>
-            <img src={Grandpianos} className='w-6 h-6'/>
-            <h1 className='text-xs font-semibold'></h1>
-        </div>
-        <div className='ml-9'>
-            <img src={Tropcal} className='w-6 h-6'/>
-            <h1 className='text-xs font-semibold'></h1>
-        </div>
-        <div className='ml-9'>
-            <img src={Bedbreakfasts} className='w-6 h-6'/>
-            <h1 className='text-xs font-semibold'></h1>
-        </div>
-        <div className='ml-9'>
-            <img src={Historicalhomes} className='w-6 h-6'/>
-            <h1 className='text-xs font-semibold'></h1>
-        </div>
-        <div className='ml-9'>
-            <img src={Offthegrid} className='w-6 h-6'/>
-            <h1 className='text-xs font-semibold'></h1>
-        </div>
-        <div className='ml-9'>
-            <img src={Treehouses} className='w-6 h-6'/>
-            <h1 className='text-xs font-semibold'></h1>
-        </div>
-        <div className='ml-9'>
-            <img src={Lakefront} className='w-6 h-6'/>
-            <h1 className='text-xs font-semibold'></h1>
-        </div>
-        <div className='ml-9'>
-            <img src={New} className='w-6 h-6'/>
-            <h1 className='text-xs font-semibold'></h1>
-        </div>
-        <div className='ml-9'>
-            <img src={Mansions} className='w-6 h-6'/>
-            <h1 className='text-xs font-semibold'></h1>
-        </div>
-        <div className='ml-9'>
-            <img src={Topcities} className='w-6 h-6'/>
-            <h1 className='text-xs font-semibold'></h1>
-        </div>
-        <div className='ml-9'>
-            <img src={Vineyards} className='w-6 h-6'/>
-            <h1 className='text-xs font-semibold'></h1>
-        </div>
-        <div className='ml-9'>
-            <img src={Luxe} className='w-6 h-6'/>
-            <h1 className='text-xs font-semibold'></h1>
-        </div>
-        <div className='ml-9'>
-            <img src={Creativespaces} className='w-6 h-6'/>
-            <h1 className='text-xs font-semibold'></h1>
-        </div>
-        <div className='ml-9'>
-            <img src={Islands} className='w-6 h-6'/>
-            <h1 className='text-xs font-semibold'></h1>
-        </div>
-        <div className='ml-9'>
-            <img src={Ryokans} className='w-6 h-6'/>
-            <h1 className='text-xs font-semibold'></h1>
-        </div>
-        <div className='ml-9'>
-            <img src={Houseboats} className='w-6 h-6'/>
-            <h1 className='text-xs font-semibold'></h1>
-        </div>
-        <div className='ml-9'>
-            <img src={Caves} className='w-6 h-6'/>
-            <h1 className='text-xs font-semibold'></h1>
-        </div>
-        <div className='ml-9'>
-            <img src={Boats} className='w-6 h-6'/>
-            <h1 className='text-xs font-semibold'></h1>
-        </div>
-        <div className='ml-9'>
-            <img src={Domes} className='w-6 h-6'/>
-            <h1 className='text-xs font-semibold'></h1>
-        </div>
-        <div className='ml-9'>
-            <img src={Aframes} className='w-6 h-6'/>
-            <h1 className='text-xs font-semibold'></h1>
-        </div>
-        <div className='ml-9'>
-            <img src={Arctic} className='w-6 h-6'/>
-            <h1 className='text-xs font-semibold'></h1>
-        </div>
-        <div className='ml-9'>
-            <img src={Containers} className='w-6 h-6'/>
-            <h1 className='text-xs font-semibold'></h1>
-        </div>
-        <div className='ml-9'>
-            <img src={Skiinout} className='w-6 h-6'/>
-            <h1 className='text-xs font-semibold'></h1>
-        </div>
-        <div className='ml-9'>
-            <img src={Golfing} className='w-6 h-6'/>
-            <h1 className='text-xs font-semibold'></h1>
-        </div>
-        <div className='ml-9'>
-            <img src={Towers} className='w-6 h-6'/>
-            <h1 className='text-xs font-semibold'></h1>
-        </div>
-        <div className='ml-9'>
-            <img src={Earthhomes} className='w-6 h-6'/>
-            <h1 className='text-xs font-semibold'></h1>
-        </div>
-        <div className='ml-9'>
-            <img src={Topoftheworld} className='w-6 h-6'/>
-            <h1 className='text-xs font-semibold'></h1>
-        </div>
-        <div className='ml-9'>
-            <img src={Surfing} className='w-6 h-6'/>
-            <h1 className='text-xs font-semibold'></h1>
-        </div>
-    </div>
-  )
-}
+    <Slider {...settings}> {/* 수정: slider를 Slider로 변경 */}
+      <div className='ml-9'>
+        <img src={Countryside} className='w-6 h-6' alt='Countryside' />
+        <h1 className='text-xs font-semibold'>Countryside</h1>
+      </div>
+      <div className='ml-9'>
+        <img src={Amazingpools} className='w-6 h-6' alt='Amazing Pools' />
+        <h1 className='text-xs font-semibold'>Amazing Pools</h1>
+      </div>
+      <div className='ml-9'>
+        <img src={Nationalparks} className='w-6 h-6' alt='National Parks' />
+        <h1 className='text-xs font-semibold'>National Parks</h1>
+      </div>
+      {/* Add other carousel items similarly */}
+    </Slider>
+  );
+};
+
+
